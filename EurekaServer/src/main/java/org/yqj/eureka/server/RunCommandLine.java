3a279698a3fb99e352ce21c1585a334ec58253b8
qijunyao <qijunyao@xiaohongshu.com>
2018-11-23 10:59:14 +0800

    add server peer conodition

--- EurekaServer/src/main/java/org/yqj/eureka/server/RunCommandLine.java
+++ EurekaServer/src/main/java/org/yqj/eureka/server/RunCommandLine.java
@@ -1,6 +1,5 @@
 package org.yqj.eureka.server;
 
-import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
