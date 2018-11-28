3c7f03232c0fa16185cc6c9dc81a7411c361522d
qijunyao <qijunyao@xiaohongshu.com>
2018-11-28 19:55:03 +0800

    hystrix content all

---
+++ HystrixConsumer/src/main/java/org/yqj/hystrix/consumer/HelloController.java
@@ -0,0 +1,32 @@
+package org.yqj.hystrix.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description:
+ *
+ * @author yaoqijun
+ * @date 2018/11/23
+ * Email: qijunyao@xiaohongshu.com
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    private HelloService helloService;
+
+    @Autowired
+    private FeignHelloService feignHelloService;
+
+    @RequestMapping(value = "/")
+    public String hi(){
+        return helloService.hiService();
+    }
+
+    @RequestMapping(value = "/feign")
+    public String hiWithFeign(){
+        return feignHelloService.sayHi();
+    }
+}
---
+++ HystrixConsumer/src/main/java/org/yqj/hystrix/consumer/HelloService.java
@@ -0,0 +1,23 @@
+package org.yqj.hystrix.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Description:
+ *
+ * @author yaoqijun
+ * @date 2018/11/23
+ * Email: qijunyao@xiaohongshu.com
+ */
+@Service
+public class HelloService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String hiService(){
+        return restTemplate.getForObject("http://SERVICE-HI-V1/", String.class);
+    }
+}
