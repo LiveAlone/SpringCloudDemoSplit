0cb0a1f90820003412b2757c97b11a16889306bb
qijunyao <qijunyao@xiaohongshu.com>
2018-11-30 15:18:19 +0800

    add with dash port

--- HystrixDashboard/src/main/java/org/yqj/hystrix/dashboard/BootDemoApplication.java
+++ HystrixDashboard/src/main/java/org/yqj/hystrix/dashboard/BootDemoApplication.java
@@ -1,11 +1,10 @@
-package org.yqj.hystrix.consumer;
+package org.yqj.hystrix.dashboard;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,8 +17,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
-@EnableHystrix
+@EnableHystrixDashboard
 public class BootDemoApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder()
